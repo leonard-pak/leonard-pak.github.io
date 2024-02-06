cd75ab5d9222d2e7fae5e3b80410defbf40a6bd9
Leonard Pak <leopak2000@gmail.com>
2024-02-06 14:18:45 +1000

    rename blog to projects and add contacts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,16 +3,16 @@ import { SITE_DESCRIPTION, SITE_TITLE } from "@config";
 import { getCollection } from "astro:content";
 
 export async function GET(context) {
-  const blog = await getCollection("blog");
+  const projects = await getCollection("projects");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/blog/${post.slug}/`,
+    items: projects.map((project) => ({
+      title: project.data.title,
+      pubDate: project.data.pubDate,
+      description: project.data.description,
+      link: `/blog/${project.slug}/`,
     })),
   });
 }
