8a2a4438090683c12a0c07cf455f09f1b5a60825
Leonard Pak <leopak2000@gmail.com>
2024-02-09 13:57:41 +0300

    fix sidebar

---
+++ src/lib/ui.ts
@@ -0,0 +1,14 @@
+export default {
+  home: {
+    name: "Главная",
+    url: "/",
+  },
+  projects: {
+    name: "Проекты",
+    url: "/projects",
+  },
+  cv: {
+    name: "Резюме",
+    url: "/cv",
+  },
+};
